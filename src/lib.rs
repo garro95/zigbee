75cae2fb2ec5f11ec10b19b7aad0462672e16f55
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2017-09-24 22:00:17 +0200

    First Commit!

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! This library provide an interface for the Zigbee network protocol, that
+//! provides a protocol stack to be used in internet of things applications.
+//! Its architecture aims to follow the stack as proposed in the Zigbee
+//! Specification document attached in the doc directory.
+//! MAC and PHY layers are provided by the IEEE 802.15.4-2003 standard.
+//! This library covers the upper layes as the Zigbee Standard does.
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
