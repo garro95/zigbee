88b75f314ed144d6a27ac8e2d7c956d9ec669f82
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-07-22 12:50:24 +0200

    Modified primitives to return futures

--- src/apl/device_profile.rs
+++ src/apl/device_profile.rs
@@ -1,3 +1,4 @@
+use futures::future::Future;
 use apl::AddrAndEp;
 
 pub struct DiscoveryStoreReq {
@@ -278,54 +279,60 @@ pub struct NetworkListRecord {
     permit_joining: bool
 }
 
+pub struct PowerDescStoreRsp {
+    status: Status,
+    ieee_addr: u64,
+    power_descriptor: PowerDescriptor
+}
+
 use apl::framework::{NodeDescriptor, PowerDescriptor, SimpleDescriptor,
                      ComplexDescriptor, UserDescriptor, ServerMask};
 
 pub trait DeviceProfileClient {
     //2.4.3.1 Device and Service Discovery Client Services
     //directed to remote devices => should return futures.
-    fn nwk_addr_req(ieee_address: u64, request_type: u8, start_index: u8) -> NwkAddrRsp;
-    fn ieee_addr_req(nwk_addr_of_interest: u16, request_type: u8, start_index:u8) -> IeeeAddrRsp;
-    fn node_desc_req(nwk_addr_of_interest: u16) -> NodeDescRsp;
-    fn power_desc_req(nwk_addr_of_interest: u16)-> PowerDescRsp;
-    fn simple_desc_req(nwk_addr_of_interest: u16, endpoint: u8) -> SimpleDescRsp;
-    fn active_ep_req(nwk_addr_of_interest: u16) -> ActiveEpRsp;
-    fn match_desc_req(request: MatchDescReq) -> MatchDescRsp;
-    fn complex_desc_req(nwk_addr_of_interest: u16) -> ComplexDescRsp;
-    fn user_desc_req(nwk_addr_of_interest: u16) -> UserDescRsp;
-    fn discovery_cache_req(nwk_addr: u16, ieee_addr: u64) -> Status;
+    fn nwk_addr_req(ieee_address: u64, request_type: u8, start_index: u8) -> Box<Future<Output=NwkAddrRsp>>;
+    fn ieee_addr_req(nwk_addr_of_interest: u16, request_type: u8, start_index:u8) -> Box<Future<Output=IeeeAddrRsp>>;
+    fn node_desc_req(nwk_addr_of_interest: u16) -> Box<Future<Output=NodeDescRsp>>;
+    fn power_desc_req(nwk_addr_of_interest: u16)-> Box<Future<Output=PowerDescRsp>>;
+    fn simple_desc_req(nwk_addr_of_interest: u16, endpoint: u8) -> Box<Future<Output=SimpleDescRsp>>;
+    fn active_ep_req(nwk_addr_of_interest: u16) -> Box<Future<Output=ActiveEpRsp>>;
+    fn match_desc_req(request: MatchDescReq) -> Box<Future<Output=MatchDescRsp>>;
+    fn complex_desc_req(nwk_addr_of_interest: u16) -> Box<Future<Output=ComplexDescRsp>>;
+    fn user_desc_req(nwk_addr_of_interest: u16) -> Box<Future<Output=UserDescRsp>>;
+    fn discovery_cache_req(nwk_addr: u16, ieee_addr: u64) -> Box<Future<Output=Status>>;
     fn devce_annce(nwk_addr: u16, ieee_addr:u64, capability:u8);
-    fn user_desc_set(nwk_addr_of_interest:u16, &[u8]) -> UserDescConf;
-    fn system_server_discovery_req(server_mask:u16) -> SystemServerDiscoveryRsp;
-    fn discovery_store_req(request: DiscoveryStoreReq) -> Status;
-    fn node_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: NodeDescriptor) -> Status;
-    fn power_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: PowerDescriptor);
-    fn active_ep_store_req(nwk_addr: u16, ieee_addr: u64, active_ep_list: &[u8]);
-    fn simple_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: SimpleDescriptor);
-    fn remove_node_cache_req(nwk_addr: u16, ieee_addr: u64);
-    fn find_node_cache_req(nwk_addr: u16, ieee_addr: u64) -> FindNodeCacheRsp;
-    fn extended_simple_desc_req(nwk_addr_of_interest: u16, endpoint: u8, start_index: u8) -> ExtendedSimpleDescRsp;
-    fn extended_active_ep_req(nwk_addr_of_interest: u16, start_index: u8) -> ExtendedActiveEpRsp;
+    fn user_desc_set(nwk_addr_of_interest:u16, &[u8]) -> Box<Future<Output=UserDescConf>>;
+    fn system_server_discovery_req(server_mask:u16) -> Box<Future<Output=SystemServerDiscoveryRsp>>;
+    fn discovery_store_req(request: DiscoveryStoreReq) -> Box<Future<Output=Status>>;
+    fn node_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: NodeDescriptor) -> Box<Future<Output=Status>>;
+    fn power_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: PowerDescriptor) -> Box<Future<Output=PowerDescStoreRsp>>;
+    fn active_ep_store_req(nwk_addr: u16, ieee_addr: u64, active_ep_list: &[u8]) -> Box<Future<Output=Status>>;
+    fn simple_desc_store_req(nwk_addr: u16, ieee_addr: u64, descriptor: SimpleDescriptor) -> Box<Future<Output=Status>>;
+    fn remove_node_cache_req(nwk_addr: u16, ieee_addr: u64) -> Box<Future<Output=Status>>;
+    fn find_node_cache_req(nwk_addr: u16, ieee_addr: u64) -> Box<Future<Output=FindNodeCacheRsp>>;
+    fn extended_simple_desc_req(nwk_addr_of_interest: u16, endpoint: u8, start_index: u8) -> Box<Future<Output=ExtendedSimpleDescRsp>>;
+    fn extended_active_ep_req(nwk_addr_of_interest: u16, start_index: u8) -> Box<Future<Output=ExtendedActiveEpRsp>>;
     // 2.4.3.2 End Device Bind, Bind, Unbind, and Bind Management Client Services Primitives
-    fn end_device_bind_req(request: EndDeviceBindReq) -> Status;
-    fn bind_req(request: BindReq) -> Status;
-    fn unbind_req(request: BindReq) -> Status;
-    fn bind_register_req(node_address: u64) -> BindRegisterRsp;
-    fn replace_device_req(old_address: u64, old_endpoint: u8, new_address: u64, new_endpoint: u8) -> Status;
-    fn store_bkup_bind_entry_req(request: BindReq) -> Status;
-    fn remove_bkup_bind_entry_req(request: BindReq) -> Status;
-    fn backup_bind_table_req(binding_table_entries: u16, binding_table_list: Vec<BindReq>) -> BackupBindTableRsp;
-    fn recover_bind_table_req(start_index: u16) -> RecoverBindTableRsp;
-    fn backup_source_bind_req(start_table_entries: u16, start_indes: u16, source_table_list: Vec<u64>) -> Status;
-    fn recover_source_bind_req(start_index: u16) -> RecoverSourceBindRsp;
+    fn end_device_bind_req(request: EndDeviceBindReq) -> Box<Future<Output=Status>>;
+    fn bind_req(request: BindReq) -> Box<Future<Output=Status>>;
+    fn unbind_req(request: BindReq) -> Box<Future<Output=Status>>;
+    fn bind_register_req(node_address: u64) -> Box<Future<Output=BindRegisterRsp>>;
+    fn replace_device_req(old_address: u64, old_endpoint: u8, new_address: u64, new_endpoint: u8) -> Box<Future<Output=Status>>;
+    fn store_bkup_bind_entry_req(request: BindReq) -> Box<Future<Output=Status>>;
+    fn remove_bkup_bind_entry_req(request: BindReq) -> Box<Future<Output=Status>>;
+    fn backup_bind_table_req(binding_table_entries: u16, binding_table_list: Vec<BindReq>) -> Box<Future<Output=BackupBindTableRsp>>;
+    fn recover_bind_table_req(start_index: u16) -> Box<Future<Output=RecoverBindTableRsp>>;
+    fn backup_source_bind_req(start_table_entries: u16, start_indes: u16, source_table_list: Vec<u64>) -> Box<Future<Output=Status>>;
+    fn recover_source_bind_req(start_index: u16) -> Box<Future<Output=RecoverSourceBindRsp>>;
     // 2.4.3.3 Network Management Client Services
-    fn mgmt_nwk_disc_req(scan_channels: u32, scan_duration: u8, start_index: u8) -> MgmtNwkDiscRsp;
-    fn mgmt_lqi_req(start_index: u8) -> MgmtLqiRsp;
-    fn mgmt_rtg_req(start_index: u8) -> MgmtRtgRsp;
-    fn mgmt_bind_req(start_index: u8) -> MgmtBindRsp;
-    fn mgmt_leave_req(device_address: u64, remove_children: bool, rejoin: bool) -> Status;
-    fn mgmt_direct_join_req(device_address: u64, capability_information: u8) -> Status;
-    fn mgmt_permit_joining_req(permit_duration: u8, tc_significance: bool) -> Status;
-    fn mgmt_cache_req(start_index: u8) -> MgmtCacheRsp;
-    fn mgmt_nwk_update_req(request: MgmtNwkUpdateReq) -> MgmtNwkUpdateNotify;   
+    fn mgmt_nwk_disc_req(scan_channels: u32, scan_duration: u8, start_index: u8) -> Box<Future<Output=MgmtNwkDiscRsp>>;
+    fn mgmt_lqi_req(start_index: u8) -> Box<Future<Output=MgmtLqiRsp>>;
+    fn mgmt_rtg_req(start_index: u8) -> Box<Future<Output=MgmtRtgRsp>>;
+    fn mgmt_bind_req(start_index: u8) -> Box<Future<Output=MgmtBindRsp>>;
+    fn mgmt_leave_req(device_address: u64, remove_children: bool, rejoin: bool) -> Box<Future<Output=Status>>;
+    fn mgmt_direct_join_req(device_address: u64, capability_information: u8) -> Box<Future<Output=Status>>;
+    fn mgmt_permit_joining_req(permit_duration: u8, tc_significance: bool) -> Box<Future<Output=Status>>;
+    fn mgmt_cache_req(start_index: u8) -> Box<Future<Output=MgmtCacheRsp>>;
+    fn mgmt_nwk_update_req(request: MgmtNwkUpdateReq) -> Box<Future<Output=MgmtNwkUpdateNotify>>;
 }
